dd87831a27c1db7be4a8a81f64a6d64de0813e7b
Hamish Haggerty <hamishhaggerty@Hamishs-MacBook-Air.local>
2023-02-25 02:00:23 +1100

    minor edit

--- cancer_proj/cancer_dataloading.py
+++ cancer_proj/cancer_dataloading.py
@@ -2,11 +2,10 @@
 
 # %% auto 0
 __all__ = ['colab_train_dir', 'colab_test_dir', 'local_train_dir', 'local_test_dir', 'melanoma_names_train',
-           'basalcellcarcinoma_names_train', 'actinickeratosis_names_train', 'melanoma_names_valid',
-           'basalcellcarcinoma_names_valid', 'actinickeratosis_names_valid', 'BYOL_Augs', 'TUNE_Augs', 'Val_Augs',
-           'get_file_lists', 'extract_text', 'label_func', 'get_difference', 'get_fnames_dict', 'get_data_dict',
-           'get_fnames_dls_dict', 'save_dict_to_gdrive', 'load_dict_from_gdrive', 'tensor_to_np', 'seed_everything',
-           'test_fnames', 'get_resnet_encoder', 'create_model', 'create_aug_pipelines']
+           'actinickeratosis_names_valid', 'BYOL_Augs', 'TUNE_Augs', 'Val_Augs', 'get_file_lists', 'extract_text',
+           'label_func', 'get_difference', 'get_fnames_dict', 'get_data_dict', 'get_fnames_dls_dict',
+           'save_dict_to_gdrive', 'load_dict_from_gdrive', 'tensor_to_np', 'seed_everything', 'test_fnames',
+           'get_resnet_encoder', 'create_model', 'create_aug_pipelines']
 
 # %% ../nbs/cancer_dataloading.ipynb 5
 import fastai
@@ -243,11 +242,10 @@ def test_fnames(_fnames):
 
 
 # %% ../nbs/cancer_dataloading.ipynb 21
-#the main point of this is to test that we get the same on colab as locally
-#the key is that we sort fnames first!!
-melanoma_names_train,basalcellcarcinoma_names_train,actinickeratosis_names_train = test_fnames(fnames_train)
-melanoma_names_valid,basalcellcarcinoma_names_valid,actinickeratosis_names_valid = test_fnames(fnames_valid)
+melanoma_names_train=['ISIC_0000139.jpg','ISIC_0000141.jpg','ISIC_0000142.jpg','ISIC_0000143.jpg','ISIC_0000144.jpg','ISIC_0000145.jpg','ISIC_0000146.jpg','ISIC_0000147.jpg','ISIC_0000148.jpg','ISIC_0000149.jpg','ISIC_0000150.jpg','ISIC_0000151.jpg','ISIC_0000152.jpg','ISIC_0000153.jpg','ISIC_0000154.jpg','ISIC_0000155.jpg','ISIC_0000156.jpg','ISIC_0000157.jpg','ISIC_0000158.jpg','ISIC_0000159.jpg','ISIC_0000160.jpg','ISIC_0000161.jpg','ISIC_0000162.jpg','ISIC_0000163.jpg','ISIC_0000164.jpg','ISIC_0000165.jpg','ISIC_0000166.jpg','ISIC_0000167.jpg','ISIC_0000168.jpg','ISIC_0000169.jpg','ISIC_0000170.jpg','ISIC_0000171.jpg','ISIC_0000172.jpg','ISIC_0000173.jpg','ISIC_0000174.jpg','ISIC_0000175.jpg','ISIC_0000176.jpg','ISIC_0000278.jpg','ISIC_0000279.jpg','ISIC_0000280.jpg','ISIC_0000285.jpg','ISIC_0000288.jpg','ISIC_0000289.jpg','ISIC_0000291.jpg','ISIC_0000292.jpg','ISIC_0000293.jpg','ISIC_0000294.jpg','ISIC_0000295.jpg','ISIC_0000296.jpg','ISIC_0000297.jpg','ISIC_0000298.jpg','ISIC_0000299.jpg','ISIC_0000300.jpg','ISIC_0000301.jpg','ISIC_0000302.jpg','ISIC_0000303.jpg','ISIC_0000304.jpg','ISIC_0000305.jpg','ISIC_0000306.jpg','ISIC_0000307.jpg','ISIC_0000308.jpg','ISIC_0000309.jpg','ISIC_0000310.jpg','ISIC_0000311.jpg','ISIC_0000312.jpg','ISIC_0000313.jpg','ISIC_0000314.jpg','ISIC_0000390.jpg','ISIC_0000393.jpg','ISIC_0000394.jpg','ISIC_0000395.jpg','ISIC_0000398.jpg','ISIC_0000399.jpg','ISIC_0000400.jpg','ISIC_0000401.jpg','ISIC_0000402.jpg','ISIC_0000404.jpg','ISIC_0000405.jpg','ISIC_0000406.jpg','ISIC_0000410.jpg']
 
+# %% ../nbs/cancer_dataloading.ipynb 22
+actinickeratosis_names_valid=['ISIC_0030491.jpg','ISIC_0030586.jpg','ISIC_0030655.jpg','ISIC_0030730.jpg','ISIC_0030825.jpg','ISIC_0030826.jpg','ISIC_0030877.jpg','ISIC_0031040.jpg','ISIC_0031108.jpg','ISIC_0031228.jpg','ISIC_0031292.jpg','ISIC_0031335.jpg','ISIC_0031381.jpg','ISIC_0031430.jpg','ISIC_0031506.jpg','ISIC_0031609.jpg','ISIC_0031823.jpg','ISIC_0031922.jpg','ISIC_0031993.jpg','ISIC_0032135.jpg']
 
 # %% ../nbs/cancer_dataloading.ipynb 26
 def get_resnet_encoder(model,n_in=3):
